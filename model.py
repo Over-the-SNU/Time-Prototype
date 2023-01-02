2fc7118ec8951ffcd1a43d89a2c10627dec711f6
catnip033 <henry11n@naver.com>
2023-01-03 01:30:19 +0900

    model manager added

--- model.py
+++ model.py
@@ -1,16 +1,47 @@
 import sqlite3
+from typing import ClassVar
 from dataclasses import dataclass
 from datetime import datetime
 from datetime import date
 
 
-class Repeat(dataclass):
+class Manager:
+    __object_set = set()
+
+    def __init__(self):
+        # DB에서 불러오기
+        pass
+
+    def __del__(self):
+        # DB에 저장
+        pass
+
+    def all(self):
+        pass
+
+    def get(self, id):
+        pass
+
+    def create(self, obj):
+        pass
+
+    def update(self, id, obj):
+        pass
+
+    def delete(self, id):
+        pass
+
+
+@dataclass
+class Repeat:
     day: int  # bit mask
     week_interval: int
     due: date
 
 
-class Todo(dataclass):
+@dataclass
+class Todo:
+    objects: ClassVar[Manager] = Manager()
     id: int
     name: str
     done: bool
@@ -21,7 +52,9 @@ class Todo(dataclass):
     content: str
 
 
-class CalenderEvent(dataclass):
+@dataclass
+class CalenderEvent:
+    objects: ClassVar[Manager] = Manager()
     id: int
     name: str
     from_time: datetime
