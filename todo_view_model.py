eb25fb5a6743096c7301dd20c6df4afdd90d6d2b
bean109 <auddl1020@gmail.com>
2023-01-05 22:42:39 +0900

    todo list

---
+++ todo_view_model.py
@@ -0,0 +1,46 @@
+from model import Todo
+from constants import *
+
+
+class TodoViewModel():
+
+    def __init__(self):
+        pass
+
+    def validate(self, obj):
+
+        if obj.name == '':
+            return CODE_TITLE_EMPTY
+
+        if obj.progress < 0:
+            return CODE_INVALID_PROGRESS
+
+        if obj.importance < 0:
+            return CODE_INVALID_IMPORTANCE
+
+        return CODE_SUCCESS
+
+    def create(self, date, name, content, **kwargs):
+
+        id = 0
+        # Todo: id 변경
+        repeat = kwargs.get("repeat")
+        done = kwargs.get("done")
+        progress = kwargs.get("progress")
+        importance = kwargs.get("importance")
+
+        obj = Todo(id=id, name=name, date=date, content=content, repeat=repeat, done=done, progress=progress,
+                   importance=importance)
+
+        if self.validate(obj) == CODE_SUCCESS:
+            Todo.objects.create(obj)
+        else:
+            return self.validate(obj)
+
+    def get_list(self, **kwargs):
+
+        l = []
+        for obj in Todo.objects.all():
+            if all(getattr(obj, attr) == value for attr, value in kwargs.items()):
+                l.append(obj)
+        return l
