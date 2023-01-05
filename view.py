c497d555c5adb1f3c3b3c0c8283b102a01207b23
catnip033 <henry11n@naver.com>
2023-01-06 01:39:59 +0900

    schedule detail view done

--- view.py
+++ view.py
@@ -13,8 +13,7 @@ class ScheduleDetailView:
     def load(self):
         print(f'일정 [{self.id}]')
 
-        while True:
-            self.retrieve()
+        while self.retrieve():
 
             print('1. 편집')
             print('2. 삭제')
@@ -29,11 +28,13 @@ class ScheduleDetailView:
             if choice == 3:
                 break
 
+            print()
+
     def retrieve(self):
         obj = schedule_view_model.get_detail(self.id)
         if obj is None:
-            print('해당 id를 가진 일정이 존재하지 않습니다.')
-            return
+            print('존재하지 않는 일정입니다.')
+            return False
 
         print(f'제목: {obj.name}')
         print(f'시작 시간: {obj.from_time.strftime("%Y-%m-%d %H:%M")}')
@@ -53,7 +54,14 @@ class ScheduleDetailView:
         if obj.importance:
             print(f'중요도: {obj.importance}')
 
+        return True
+
     def update(self):
+        obj = schedule_view_model.get_detail(self.id)
+        if obj is None:
+            print('존재하지 않는 일정입니다.')
+            return
+
         print(f'수정할 항목을 선택하세요.')
         print('1. 제목')
         print('2. 시간')
@@ -62,44 +70,79 @@ class ScheduleDetailView:
         print('5. 중요도')
         choice = int(input('>>> '))
 
+        code = None
+
         if choice == 1:
             name = input('새 제목: ')
-            schedule_view_model.update(self.id, name=name)
+            code = schedule_view_model.update(self.id, name=name)
 
         if choice == 2:
             from_time = datetime.strptime(input('새 시작 시간 (yyyy-mm-dd HH:MM): '), '%Y-%m-%d %H:%M')
             to_time = datetime.strptime(input('새 종료 시간 (yyyy-mm-dd HH:MM): '), '%Y-%m-%d %H:%M')
-            schedule_view_model.update(self.id, from_time=from_time, to_time=to_time)
+            code = schedule_view_model.update(self.id, from_time=from_time, to_time=to_time)
 
         if choice == 3:
             content = input('새 내용: ')
-            schedule_view_model.update(self.id, content=content)
+            code = schedule_view_model.update(self.id, content=content)
 
         if choice == 4:
-            print('새 반복')
-            day = sum(1 << day_str.index(d) for d in input('요일 (콤마로 구분): ').split(','))
-            week_interval = int(input('반복 간격 (주): '))
-            due = datetime.strptime(input('만료 날짜 (yyyy-mm-dd): '), '%Y-%m-%d').date()
-            schedule_view_model.update(self.id, repeat=Repeat(day=day, week_interval=week_interval, due=due))
+            remove_repeat = False
+            if obj.repeat:
+                print('기존 반복이 있습니다.')
+                print('1. 반복 편집')
+                print('2. 반복 제거')
+                if int(input('>>> ')) == 2:
+                    remove_repeat = True
+
+            if remove_repeat:
+                code = schedule_view_model.update(self.id, repeat=None)
+            else:
+                print('새 반복')
+                day = sum(1 << day_str.index(d) for d in input('요일 (콤마로 구분): ').split(','))
+                week_interval = int(input('반복 간격 (주): '))
+                due = datetime.strptime(input('만료 날짜 (yyyy-mm-dd): '), '%Y-%m-%d').date()
+                code = schedule_view_model.update(self.id, repeat=Repeat(day=day, week_interval=week_interval, due=due))
 
         if choice == 5:
-            importance = int(input('새 중요도: '))
-            schedule_view_model.update(self.id, importance=importance)
+            remove_importance = False
+            if obj.importance:
+                print('기존 중요도가 있습니다.')
+                print('1. 중요도 편집')
+                print('2. 중요도 제거')
+                if int(input('>>> ')) == 2:
+                    remove_importance = True
+
+            if remove_importance:
+                code = schedule_view_model.update(self.id, importance=0)
+            else:
+                importance = int(input('새 중요도: '))
+                code = schedule_view_model.update(self.id, importance=importance)
+
+        if code == CODE_SUCCESS:
+            print('일정이 수정되었습니다.')
+        elif code == CODE_INVALID_DATE:
+            print('유효하지 않은 시작 시간과 종료 시간입니다.')
+        elif code == CODE_TITLE_EMPTY:
+            print('제목이 비어 있습니다.')
+        elif code == CODE_ID_NOT_FOUND:
+            print('존재하지 않는 일정입니다.')
+        else:
+            print(f'내부 오류로 실패하였습니다. ({code})')
 
     def destroy(self):
         code = schedule_view_model.delete(self.id)
 
         if code == CODE_ID_NOT_FOUND:
-            print('해당 id를 가진 일정이 존재하지 않습니다.')
+            print('존재하지 않는 일정입니다.')
             return
 
         print('일정이 삭제되었습니다.')
 
 
-assert schedule_view_model.create(from_time=datetime.now(),
-                                  to_time=datetime.now() + timedelta(days=1),
-                                  name='오더스 프로토타입 개발',
-                                  content='캘린더 디테일 뷰 구현') == CODE_SUCCESS
+# schedule_view_model.create(from_time=datetime.now(),
+#                                   to_time=datetime.now() + timedelta(days=1),
+#                                   name='오더스 프로토타입 개발',
+#                                   content='캘린더 디테일 뷰 구현')
 
 print(Schedule.objects.all())
 
